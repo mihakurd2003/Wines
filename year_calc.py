1da605cce6e60b910102e06243ccc76fc0a0f55e
S1im_Sh4dy <mihakurd@gmail.com>
2022-11-30 23:01:12 +0300

    fixes

--- year_calc.py
+++ year_calc.py
@@ -1,22 +1,24 @@
 import datetime
 
+date_now = datetime.datetime.now()
+years_old = date_now.year - 1920
 
-def validate_form(num) -> str:
-    dict_of_end_numbers = {
+
+def get_number_end(num) -> str:
+    end_numbers = {
         'год': [1],
         'года': [2, 3, 4],
         'лет': [11, 12, 13, 14]  # Exceptions from russian language:)
     }
-    if num % 100 in dict_of_end_numbers['лет']:
+    if num % 100 in end_numbers['лет']:
         return 'лет'
 
-    if num % 10 in dict_of_end_numbers['год']:
+    if num % 10 in end_numbers['год']:
         return 'год'
-    elif num % 10 in dict_of_end_numbers['года']:
+    elif num % 10 in end_numbers['года']:
         return 'года'
 
     return 'лет'
 
 
-date_now = datetime.datetime.now()
-years_old = date_now.year - 1920
+
