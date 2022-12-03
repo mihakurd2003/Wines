36e258606c8d78934ae90a1fb9dfad6fa543aa23
S1im_Sh4dy <mihakurd@gmail.com>
2022-12-03 18:23:22 +0300

    fixes again

--- collect_wines.py
+++ collect_wines.py
@@ -5,11 +5,10 @@ from collections import defaultdict
 
 def wines_processing(path):
     wines_excel = pd.read_excel(path)
-    wines_json = json.loads(wines_excel.to_json(orient='records', force_ascii=False))
+    wines_json = wines_excel.to_dict(orient='records')
 
     wines = defaultdict(list)
     for wine in wines_json:
         wines[wine['Категория']] = [item for item in wines_json if item['Категория'] == wine['Категория']]
 
     return wines
-
